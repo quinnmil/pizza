c86ef650df3861f66e122d075b8d497dcdcc0edf
m <39255541+margielamadman@users.noreply.github.com>
2023-04-27 21:23:24 -0700

    Add chat frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { initialMessage } from './initialMessage'
@@ -5,144 +6,119 @@ import { ChatCompletionRequestMessage, ChatCompletionRequestMessageRoleEnum } fr
 
 const inter = Inter({ subsets: ['latin'] })
 
-let messages = [initialMessage]
+// let messages = [initialMessage]
 
-const sendChat = async (message: string) => {
-  const newMessageObject: ChatCompletionRequestMessage = {
-    role: "user", 
-    content: message
-  }
-  messages = [...messages, newMessageObject]
-  const res = await fetch("/api/chat", {
-    method: "POST", 
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      messages
-    })
-  })
-  const responseMessageObject: ChatCompletionRequestMessageRoleEnum = {
-    role: res.role,
+// const sendChat = async (message: string) => {
+//   const newMessageObject: ChatCompletionRequestMessage = {
+//     role: "user", 
+//     content: message
+//   }
+//   messages = [...messages, newMessageObject]
+//   const res = await fetch("/api/chat", {
+//     method: "POST", 
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       messages
+//     })
+//   })
+//   const responseMessageObject: ChatCompletionRequestMessageRoleEnum = {
+//     role: res.role,
 
-  }
-  messages = [
-    ...messages,
-    res
-  ]
+//   }
+//   messages = [
+//     ...messages,
+//     res
+//   ]
+// }
+
+interface ChatProps {
+  children: React.ReactNode;
 }
 
-
-export default function Home() {
+const Chat: React.FC<ChatProps> = ({ children }) => {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col flex-grow h-0 p-4 overflow-auto">
+      {children}
+    </div>
+  );
+};
+
+interface MessageProps {
+  message: string;
+  role: 'gpt' | 'user';
+}
+
+const GPTMessage: React.FC<MessageProps> = ({ message }) => {
+  return (
+    <div className="flex w-full mt-2 space-x-3 max-w-xs">
+      <div>
+        <div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+          <p className="text-sm">{message}</p>
         </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+const UserMessage: React.FC<MessageProps> = ({ message }) => {
+  return (
+    <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+      <div>
+        <div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+          <p className="text-sm">{message}</p>
+        </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+const Input: React.FC<{ onSubmit: (message: string, role: 'gpt' | 'user') => void; role: 'gpt' | 'user' }> = ({ onSubmit, role }) => {
+  const [inputValue, setInputValue] = useState('');
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    onSubmit(inputValue, role);
+    setInputValue('');
+  };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+  return (
+    <form className="bg-gray-300 p-4" onSubmit={handleSubmit}>
+      <input
+        className="flex items-center h-10 w-full rounded px-3 text-black text-sm"
+        type="text"
+        placeholder="Type your message…"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+    </form>
+  );
+};
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+export default function Home() {
+  const [messages, setMessages] = useState<MessageProps[]>([
+    { message: 'Hello, this is a GPT message.', role: 'gpt' },
+    { message: 'Hello, this is a User message.', role: 'user' },
+  ]);
+
+  const handleNewMessage = (message: string, role: 'gpt' | 'user') => {
+    setMessages([...messages, { message, role }]);
+  };
+
+  return (
+    <main className={`flex min-h-screen flex-col flex-grow w-full max-w-full bg-white shadow-xl rounded-lg justify-between p-24 ${inter.className}`}>
+      <Chat>
+        {messages.map((msg, index) =>
+          msg.role === 'gpt' ? (
+            <GPTMessage key={index} message={msg.message} role={msg.role} />
+          ) : (
+            <UserMessage key={index} message={msg.message} role={msg.role} />
+          ),
+        )}
+      </Chat>
+
+      <Input onSubmit={handleNewMessage} role="user" />
     </main>
-  )
+  );
 }
